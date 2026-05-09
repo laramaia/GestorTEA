a35f61d6c6e650d0a8810bd62f472d30c67db8ea
THAÍS VITÓRIA <102629334+ThaisVDR@users.noreply.github.com>
2026-05-09 19:32:00 -0400

    subindo frontend

--- frontend/src/components/Button/Button.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import style from "./button.module.css";
-
-function Button({ texto }) {
-  return <button className={style.button}>{texto}</button>;
-}
-
-export default Button;
--- frontend/src/services/api.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-const api = axios.create({
-  // Verifique se não há espaços extras aqui
-  // O final "api" deve bater com a [Route("api/[controller]")] do C#
-  baseURL: "https://localhost:7230/api",
-});
-
-export default api;
