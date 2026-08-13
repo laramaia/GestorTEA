83a386076bd62d628c9a0ca948a0539ad823661d
Lara Caroline Monteiro Maia <laramaia@MacBook-Air-de-Lara.local>
2026-08-13 09:47:22 -0400

    Adiciona configs na API

--- frontend/frontend/src/services/api.ts
+++ frontend/frontend/src/services/api.ts
@@ -1,6 +1,13 @@
 import axios from "axios";
-const BASE_URL =
-  import.meta.env.VITE_API_URL || "https://gestortea.onrender.com/api";
+
+const CONFIGS = {
+  RIDER: "http://localhost:5055/api",
+  VISUAL_STUDIO: "https://localhost:7230/api",
+  RENDER: "https://gestortea.onrender.com/api"
+};
+
+
+const BASE_URL = import.meta.env.VITE_API_URL || CONFIGS.RIDER;
 
 const api = axios.create({
   baseURL: BASE_URL,
